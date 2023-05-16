1f388123e1a08b6b4564261dfc50bc4070576e83
Gabriel Vasconcelos <97545157+gabriel-vasconcelos-ft@users.noreply.github.com>
2023-05-16 14:27:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#url = 'https://docs.google.com/spreadsheets/d/1Xr6uPNAlT4n9FDuR91JyY2QwFGLGmMICPE5WqjW-s1k/edit#gid=1378939680' 
+#df = pd.read_csv(url)
+#st.write(df)
+
+filename = "relatorio-sisref-sad-relatorio-sisref-sad.csv"
+st.dataframe(pd.read_csv(filename))
+
 
